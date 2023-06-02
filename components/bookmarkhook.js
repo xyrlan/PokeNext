0b39be7373dfda869f8497b6eb15b665ccde19c2
xyrlan; pedro <lobatopedro68@gmail.com>
2023-06-02 19:43:59 -0300

    try to fix bookmarkhook

--- components/bookmarkhook.js
+++ components/bookmarkhook.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-
 import { useUser } from '@auth0/nextjs-auth0/client';
 
 const useBookmark = () => {
@@ -18,16 +17,33 @@ const useBookmark = () => {
   useEffect(() => {
     // Carregar os dados do usuário, se estiver autenticado
     if (isAuthenticated) {
-      setselectedPokemon(user.selectedPokemon || []);
+      fetch('/api/user/pokemon') // Substitua a URL pela rota correta da sua API
+        .then(response => response.json())
+        .then(data => {
+          setselectedPokemon(data.selectedPokemon || []);
+        })
+        .catch(error => {
+          console.error('Erro ao carregar os Pokémons marcados:', error);
+        });
     }
-  }, [isAuthenticated, user]);
+  }, [isAuthenticated]);
 
   useEffect(() => {
     // Atualizar os dados do usuário, se estiver autenticado
     if (isAuthenticated) {
-      user.update({ selectedPokemon });
+      fetch('/api/user/pokemon', {
+        method: 'POST', // Ou o método adequado para atualização na sua API
+        body: JSON.stringify({ selectedPokemon }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .catch(error => {
+          console.error('Erro ao atualizar os Pokémons marcados:', error);
+        });
     }
-  }, [selectedPokemon, isAuthenticated, user]);
+  }, [selectedPokemon, isAuthenticated]);
 
   return { selectedPokemon, handleBookmark };
 };
